16d447a8dbb03607cdf3bd4040b4d32436e88d61
Thomas Stache <dev@thomasstache.de>
2020-11-01 19:05:58 +0100

    Step 10: Testing same-thread operations in Compose State

--- StateCodelab/start/src/test/java/com/codelabs/state/todo/TodoViewModelTest.kt
+++ StateCodelab/start/src/test/java/com/codelabs/state/todo/TodoViewModelTest.kt
@@ -16,6 +16,63 @@
 
 package com.codelabs.state.todo
 
+import com.codelabs.state.util.generateRandomTodoItem
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
 class TodoViewModelTest {
-    // TODO: Write tests
+    @Test
+    fun whenRemovingItem_updatesList() {
+        val subject = TodoViewModel()
+        val item1 = generateRandomTodoItem()
+        val item2 = generateRandomTodoItem()
+        subject.addItem(item1)
+        subject.addItem(item2)
+
+        subject.removeItem(item1)
+
+        assertThat(subject.todoItems).isEqualTo(listOf(item2))
+    }
+
+    @Test
+    fun whenNotEditing_currentEditItemIsNull() {
+        val subject = TodoViewModel()
+        val item = generateRandomTodoItem()
+        subject.addItem(item)
+
+        assertThat(subject.currentEditItem).isNull()
+    }
+
+    @Test
+    fun whenEditing_currentEditItemIsCorrectItem() {
+        val subject = TodoViewModel()
+        val item1 = generateRandomTodoItem()
+        val item2 = generateRandomTodoItem()
+        subject.addItem(item1)
+        subject.addItem(item2)
+        subject.onEditItemSelected(item1)
+
+        assertThat(subject.currentEditItem).isEqualTo(item1)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun whenEditing_wrongItemThrows() {
+        val subject = TodoViewModel()
+        val item1 = generateRandomTodoItem()
+        val item2 = generateRandomTodoItem()
+        subject.addItem(item1)
+        subject.addItem(item2)
+        subject.onEditItemSelected(item1)
+
+        val expected = item2.copy("update for test case")
+        subject.onEditItemChange(expected)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun whenNotEditing_onEditItemChangeThrows() {
+        val subject = TodoViewModel()
+        val item = generateRandomTodoItem()
+
+        subject.onEditItemChange(item)
+    }
 }
